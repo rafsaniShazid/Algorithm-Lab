0c3c9dcaa93d0e7daeed56203885bdfc39fd58f8
Md. Rafsani Shazid <rafsanishazid@gmail.com>
2024-10-09 19:15:59 +0600

    Create floyd_warshall.cpp

---
+++ floyd_warshall.cpp
@@ -0,0 +1,29 @@
+//User function template for C++
+
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    int n= matrix[0].size();
+	   for(int i=0;i<n;i++){
+	       for(int j=0;j<n;j++){
+	           if(matrix[i][j]==-1) matrix[i][j]=1e9;
+	           if(i==j)matrix[i][j]=0;
+	       }
+	   }
+	   for(int k=0;k<n;k++){
+	       for(int i=0;i<n;i++){
+	           for(int j=0;j<n;j++){
+	               matrix[i][j]= min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+	           }
+	       }
+	   }
+	   	   for(int i=0;i<n;i++){
+	       for(int j=0;j<n;j++){
+	           if(matrix[i][j]==1e9) matrix[i][j]=-1;
+	       }
+	   }
+	   
+	   
+	}
+};
+//https://www.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1
